0edf08911efaf919f879d1f23162d3e581fc8fac
AndreXime <andreximenes536@gmail.com>
2024-07-08 22:05:09 -0300

    csszin

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     class Meta:
@@ -20,6 +21,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=False)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/%d')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
-    
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+
     def __str__(self) -> str:
         return f'{self.category} - {self.firstName}'
